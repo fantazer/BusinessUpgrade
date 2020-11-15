21019f98dd3f9d22d3a5b3a850fa487b7c43b766
FANTAZER <kuznecovs45@gmail.com>
2020-11-15 17:58:23 +0300

    deploy version

---
+++ dist/js/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+
+// slider
+$('.b-slider-items').slick({
+		slidesToShow: 2,
+		slidesToScroll: 2,
+		speed: 500,
+		dots:true,
+		arrows:false,
+		rows:0,
+		adaptiveHeight: true,
+		responsive: [
+
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: false,
+        slidesToShow: 1,
+				slidesToScroll:1,
+      }
+    }
+  ],
+		customPaging : function(slider, i) {
+			return '<span class="dot"></span>';
+		}
+	}).on('setPosition', function (event, slick) {
+    slick.$slides.css('height', slick.$slideTrack.height() + 'px');
+	});
+
+	// === custom arrow el ===
+	$('.b-slider-control--right').click(function(){
+		$(this).closest(".b-slider-wrap").find(".b-slider-item").slick('slickNext');
+	});
+
+	$('.b-slider-control--left').click(function(){
+		$(this).closest(".b-slider-wrap").find(".b-slider-item").slick('slickPrev');
+	});
+	// custom arrow el === end
+
+
+// slider === end
+
+
+});
